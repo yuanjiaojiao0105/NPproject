0a6e6bdb72dddec19ce8c65ec50d11c0c93a885e
yuanjiaojiao <18638520107@163.com>
2020-12-16 12:02:38 +0800

    修改app.vue文件

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
+
+      <div>哈哈</div>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
